a778723d6b4a093545436d86ad9a03e42f6d8c62
Kyle Fox <kyle.fox@gmail.com>
2009-10-26 13:04:10 -0600

    Started credit card validation.

--- billing/credit_card.py
+++ billing/credit_card.py
@@ -1,5 +1,6 @@
 import re
 from merchant.billing.credit_card_validator import CreditCardValidator
+from merchant.billing.expiry_date import ExpiryDate
 
 class CreditCard(object):
     
@@ -10,21 +11,27 @@ class CreditCard(object):
 
     def __init__(self, number, month, year, first_name, last_name):
       self.number = re.sub('[^\d]', '', str(number))
-      self.month = int(month)
-      self.year = int(year)
+      self.expiry_date = ExpiryDate(month, year)
       self.first_name = first_name
       self.last_name = last_name
       self.validator = CreditCardValidator(self)
      
     @classmethod
     def type(cls, number):
-        """
-        Returns a string indicating the issuing company of the `number`.
-        """
+        "Returns a string indicating the issuing company of the `number`."
         for name, cc_re in cls.CARD_COMPANIES.items():
             if re.compile(cc_re).match(number):
                 return name
         return 'unknown'
+
+    def is_expired(self):
+        "Returns True/False indicating if this card has expired."
+        return self.expiry_date.is_expired()
+    
+    def validate(self):
+        return len(self.errors) == 0
         
-    # def type(self):
-    #     return CreditCard.type(self.number)
+    @property
+    def errors(self):
+        self.validator.validate()
+        return self.validator.errors
--- billing/credit_card_validator.py
+++ billing/credit_card_validator.py
@@ -1,4 +1,21 @@
 class CreditCardValidator(object):
  
-    def __init__(self, card):
+    def __init__(self, credit_card):
         self.credit_card = credit_card
+        
+    def validate(self):
+        self.errors = {}
+        self._validate_number_length()
+        return len(self.errors) == 0
+        
+    def _validate_number_length(self):
+        if len(self.credit_card.number) < 12:
+            self._add_error(error_message=u'too short', field_name='number')
+            return False
+        return True
+            
+    def _add_error(self, error_message, field_name=None):
+        if field_name is None:
+            field_name = '_global'
+        self.errors[field_name] = self.errors.get(field_name, [])
+        self.errors[field_name].append(error_message)
